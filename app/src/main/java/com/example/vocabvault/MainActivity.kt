c2fccd7fd0c1056e50f7625260d7e9bcb4db7eb5
KyryloMuzychka <kyryll2016@gmail.com>
2023-10-15 14:08:43 +0300

    Lab4

--- app/src/main/java/com/example/vocabvault/MainActivity.kt
+++ app/src/main/java/com/example/vocabvault/MainActivity.kt
@@ -1,93 +1,50 @@
 package com.example.vocabvault
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ListView
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.vocabvault.databinding.ActivityMainBinding
 import com.example.vocabvault.domain.WordAdapter
 import com.example.vocabvault.domain.WordFactory
 
+
 class MainActivity : AppCompatActivity() {
-
-
-
-    private var windowDisplayed = false
-
-    private lateinit var addButton: Button
-    private lateinit var listView: ListView
-    private lateinit var background: View
-    private lateinit var translatedWordEdit: EditText
-    private lateinit var originalWordEdit: EditText
 
     private lateinit var mainViewModel : MainViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        binding.lifecycleOwner = this
-
-        binding.viewModel = this.mainViewModel
-
-
-
-        Initialize()
-        setupUI()
-
+        initialize()
     }
 
-    private fun Initialize() {
-        addButton = findViewById<Button>(R.id.addButton)
-        listView = findViewById<ListView>(R.id.wordsListView)
-        background = findViewById<View>(R.id.background)
-        translatedWordEdit = findViewById<EditText>(R.id.translatedWordEdit)
-        originalWordEdit = findViewById<EditText>( R.id.originalWordEdit)
+    private fun initialize() {
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = mainViewModel
+
+        binding.translatedWordEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                mainViewModel.updateTranslatedWord(s.toString()) // Update LiveData when the text changes.
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        binding.originalWordEdit.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                mainViewModel.updateOriginalWord(s.toString()) // Update LiveData when the text changes.
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
 
         WordFactory.adapter = WordAdapter(this, R.layout.list_item_layout, WordFactory.dictionary.getAllWords())
-        listView.adapter = WordFactory.adapter
+        binding.wordsListView.adapter = WordFactory.adapter
     }
 
-    private fun setupUI() {
-        addButton.setOnClickListener {
-            if (windowDisplayed) {
-                val originalWord = originalWordEdit.getText().toString()
-                val translatedWord = translatedWordEdit.getText().toString()
-                addWord(originalWord, translatedWord)
-                clearEdits()
-                hideEdits()
-                windowDisplayed = false
-            } else {
-                showEdits()
-                windowDisplayed = true
-            }
-        }
-    }
-
-    private fun addWord(originalWord: String, translatedWord: String) {
-        WordFactory.insert.addWord(WordFactory.dictionary, originalWord, translatedWord)
-        WordFactory.adapter.notifyDataSetChanged()
-    }
-
-    private fun clearEdits() {
-        originalWordEdit.text.clear()
-        translatedWordEdit.text.clear()
-    }
-
-    private fun hideEdits() {
-        mainViewModel.updateVisibility(View.INVISIBLE)
-    }
-
-    private fun showEdits() {
-        mainViewModel.updateVisibility(View.VISIBLE)
-    }
 }
